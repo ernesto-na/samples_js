d3ba694738fae1c3b75b763fa7c82222964ca48d
ernesto <ernesto-na@outlook.com>
2018-07-16 23:18:57 -0500

    15 completo

--- hola-mundo/15-ejercicio5.js
+++ hola-mundo/15-ejercicio5.js
@@ -2,16 +2,22 @@
 //Se pide el número y la tabla
 'use strict'
 
-
-do
+//var todasTablas = 1;
+//do
+//{
+		//var tabla = parseInt(prompt("Escribe la tabla: ",1));
+		//console.log(tabla);
+		
+for(var j=1; j<=10; j++)
 {
-		var tabla = parseInt(prompt("Escribe la tabla: ",1));
-		console.log(tabla);
-		document.write("La tabla del: "+tabla +"<br>");
 
+	document.write("La tabla del: "+j +"<br>");
 		for(var i=0; i<=10; i++)
 		{
-			document.write(tabla +" x "+ i+"="+tabla * i +"<br>");
+			document.write(j +" x "+ i+"="+j * i +"<br>");
 		}
+		document.write("<br>");
+//
+}		
 		var res = prompt("¿Continuar?","s")
-}while(res!="n")
+//}while(res!="n")

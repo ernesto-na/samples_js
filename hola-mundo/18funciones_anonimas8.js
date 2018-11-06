0464929b13df1d8d862ada8dc9594a943880d799
ernesto <ernesto-na@outlook.com>
2018-11-06 03:38:49 -0600

    se agregan funciones anonimas

--- hola-mundo/18funciones_anonimas8.js
+++ hola-mundo/18funciones_anonimas8.js
@@ -5,3 +5,25 @@ var pelicula = function(nombre)
 	return "La pelicula es "+nombre;
 }
 
+/*function sumame(numero1 , numero2){
+
+	var sumar = numero1+ numero2;
+	return sumar;
+}
+
+console.log(sumame(4,5));*/
+
+function sumame(numero1 , numero2,sumaYmuestra, sumaPorDos){
+
+	var sumar = numero1+ numero2;
+	sumaYmuestra(sumar)
+	sumaPorDos(sumar)
+	return sumar;
+}
+sumame(5,7,function(dato){
+	console.log("La suma es: " , dato);
+
+},
+function(dato){
+	console.log("La suma por dos es: ",(dato*2));
+});

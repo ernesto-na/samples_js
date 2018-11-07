8e4752f92af189f9999010b36fddd5e745bad3fc
“ernesto” <ernesto-na@outlook.com>
2018-11-06 22:02:04 -0600

    se agrega leccion 19

---
+++ hola-mundo/19funciones_texto.js
@@ -0,0 +1,19 @@
+'use strict'
+var numero = 44;
+var texto1 = "Bienvenido al curso de Javascript de Ernesto Nepomuceno";
+var texto2 = "es muy buen curso";
+
+var dato =numero.toString();
+
+dato = texto1.toUpperCase();
+console.log(typeof dato)
+
+console.log(dato)
+//Calcular longitud
+var nombre ="Ernesto Nepomuceno";
+nombre = ["hola","mundo"];
+console.log(nombre.length);
+
+var TextoTotal = texto1+" "+ texto2;
+
+console.log(TextoTotal);

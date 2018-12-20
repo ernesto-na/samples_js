5383d139700dd3cf44e4b30890ce93177f319d8c
“ernesto” <ernesto.na89@gmail.com>
2018-12-20 15:30:10 -0600

    leccion 21 terminada

--- hola-mundo/21arrays_multidimensionales.js
+++ hola-mundo/21arrays_multidimensionales.js
@@ -5,4 +5,14 @@ var peliculas = ['Mulan', 'Titanic','Rocky'];
 
 var cine = [categorias,peliculas];
 
-console.log(cine[1][2]);
+//console.log(cine[1][2]);
+
+      var elemento = "";
+  do{
+
+      elemento = prompt("Introduce una pelicula");
+      peliculas.push(elemento);
+
+    }while (elemento!='ACABAR');
+
+    console.log(peliculas);

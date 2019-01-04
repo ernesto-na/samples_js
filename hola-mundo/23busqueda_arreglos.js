8a8188ea24a1a2c9184cf06933714d4a8589d8d2
“ernesto” <ernesto.na89@gmail.com>
2019-01-03 18:11:20 -0600

    leccion busqueda

--- hola-mundo/23busqueda_arreglos.js
+++ hola-mundo/23busqueda_arreglos.js
@@ -2,6 +2,26 @@
 
 var peliculas = ['Mulan', 'Titanic','Rocky','Shreck', 'Sabrina'];
 
+function recorrerConForIn(arreglo)
+{
+  document.write("<br>");
+  document.write("Usando For In: ");
+  document.write("<br>");
+  for(let element in arreglo){
+      document.write( arreglo[element] + "<br>" );
+  }
+}
+
+recorrerConForIn(peliculas);
+
+/*var busqueda = peliculas.find(function(pelicula){
+
+console.log("-->" + pelicula);
+
+  return pelicula =="Titanic";
+});*/
 
 
-busqueda = peliculas.find("Mulan");
+
+    var busqueda = peliculas.findIndex(lenguaje => lenguaje == "Titanic");
+console.log(busqueda);

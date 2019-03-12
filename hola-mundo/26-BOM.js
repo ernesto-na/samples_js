59c3036826d8ab7e74bbe07e7a9b7ae8097f3a5e
“ernesto” <ernesto.na89@gmail.com>
2019-03-11 18:10:51 -0600

    lecciones nuevas

---
+++ hola-mundo/26-BOM.js
@@ -0,0 +1,48 @@
+'use strict'
+function redirigir(url){
+
+    window.location.href = url;
+
+}
+function redirect(url){
+    window.open(url,"","width=400,height=300");
+}
+function getBom(){
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+console.log(screen.width);
+console.log(screen.height);
+console.log(window.location);
+console.log(window.location.href);
+
+}
+getBom();
+function cambiaColor(color){
+
+  caja.style.color = color;
+}
+//var  caja = document.getElementById("micaja");
+
+var caja = document.querySelector("#micaja");
+
+caja.innerHTML = "Oracle";
+caja.style.background = "red";
+caja.style.padding="20px"
+caja.style.color="white";
+caja.className="holaclase"
+
+
+console.log(caja);
+
+//Conseguir todos los elementos por su etiqueta
+var todosLosDivs = document.getElementsByTagName('div');
+console.log(todosLosDivs);
+
+var texto1 = todosLosDivs[2];
+
+texto1.innerHTML = "cambiado por js";
+texto1.style.background = "red";
+texto1.style.color="white";
+
+console.log(texto1);
---
+++ hola-mundo/27-Eventos.js
@@ -0,0 +1,61 @@
+//Eventos del raton
+'use strict'
+  var boton = document.querySelector("#boton");
+function cambiarColor(){
+  var bg = boton.style.background;
+    if(bg == "green"){
+          bg = "red";
+    }else{
+        bg = "green";
+    }
+    return == true;
+}
+function redirigir(url){
+
+    window.location.href = url;
+
+}
+function redirect(url){
+    window.open(url,"","width=400,height=300");
+}
+function getBom(){
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+console.log(screen.width);
+console.log(screen.height);
+console.log(window.location);
+console.log(window.location.href);
+
+}
+getBom();
+function cambiaColor(color){
+
+  caja.style.color = color;
+}
+//var  caja = document.getElementById("micaja");
+
+var caja = document.querySelector("#micaja");
+
+caja.innerHTML = "Oracle";
+caja.style.background = "red";
+caja.style.padding="20px"
+caja.style.color="white";
+caja.className="holaclase"
+
+
+console.log(caja);
+
+//Conseguir todos los elementos por su etiqueta
+var todosLosDivs = document.getElementsByTagName('div');
+console.log(todosLosDivs);
+
+var texto1 = todosLosDivs[2];
+
+texto1.innerHTML = "cambiado por js";
+texto1.style.background = "red";
+texto1.style.color="white";
+
+console.log(texto1);
+
+var boton = document.querySelector("#boton")

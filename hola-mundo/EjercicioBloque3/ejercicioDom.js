ddeb05cb4a7c5c61209799bcc1c8cd4f70350cc4
“ernesto” <ernesto.na89@gmail.com>
2019-04-01 12:41:24 -0600

    se agrega primer Ejercicio Dom

--- hola-mundo/EjercicioBloque3/ejercicioDom.js
+++ hola-mundo/EjercicioBloque3/ejercicioDom.js
@@ -19,13 +19,22 @@ console.log("Inicia load");
         parrafo.append(apellidos);
         parrafo.append(edad);
         box_dashed.append(parrafo);*/
+          /* opcion 2
         var datos_usuario = [nombre,apellidos,edad];
         var indice;
         for(indice in datos_usuario){
               var parrafo = document.createElement("p");
               parrafo.append(datos_usuario[indice]);
               box_dashed.append(parrafo);
-        }
+        }   */
+        var p_nombre = document.querySelector("#p_nombre span");
+        var p_apellidos = document.querySelector("#p_apellidos span");
+        var p_edad = document.querySelector("#p_edad span");
+
+        p_nombre.innerHTML = nombre;
+        p_apellidos.innerHTML = apellidos;
+        p_edad.innerHTML = edad;
+
 
         console.log(nombre);
         console.log(apellidos);

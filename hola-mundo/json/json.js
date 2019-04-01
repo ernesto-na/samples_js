7ecd644b365dba50f2fb9e24c77fcc7ebb07bf3f
“ernesto” <ernesto.na89@gmail.com>
2019-04-01 17:32:01 -0600

    leccion json

---
+++ hola-mundo/json/json.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+var pelicula = {
+titulo:"superman",
+anio:"2018",
+pais:"USA"
+};
+
+var peliculas = [
+      {titulo:"vengadores",anio:"2019",pais:"USA"},
+      pelicula
+];
+
+console.log(peliculas);
+
+var index;
+  for(index in peliculas){
+
+
+    
+  }
